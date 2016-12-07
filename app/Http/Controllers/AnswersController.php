eee6bc5835fd2459b7cf1ed89542f1be9b582226
Yoosuf <mayoosuf@gmail.com>
2016-12-07 21:55:22 +0530

    Questions and Answers controllers ready, need to test

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class AnswersController extends Controller
 {
-    //
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+
+
+    public function index($question_id)
+    {
+    	$answers = Answer::where('status', true)->where('question_id', $question_id);
+    	
+    	$data = $answers->paginate(request()->has('limit') ? request()->get('limit') : 10 );
+    	
+    	return $data;
+    }
+
+
+    public function show($question_id, $answer_id)
+    {
+		$data = Answer::where('status', true)->where('question_id', $question_id)->where('id', $answer_id)->first();
+
+		if (count($data) < 1) {
+			return "No Record found";
+		}
+
+		return $data;
+    }
+
+    public function store()
+    {
+    	$user = auth()->user();
+
+    	$data = [
+    		'content' => request()->get('content')
+    	];
+
+		$user->questions()->answers()->create($data);
+
+		return "Data was created";
+    }
 }
--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -6,5 +6,44 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
-    //
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+
+
+    public function index()
+    {
+    	$questions = Question::where('status', true);
+    	
+    	$data = $questions->paginate(request()->has('limit') ? request()->get('limit') : 10 );
+    	
+    	return $data;
+    }
+
+
+    public function show($id)
+    {
+
+		$data = Question::where('status', true)->where('id', $id)->first();
+
+		if (count($data) < 1) {
+			return "No Record found";
+		}
+
+		return $data;
+    }
+
+    public function store()
+    {
+    	$user = auth()->user();
+
+    	$data = [
+    		'content' => request()->get('content')
+    	];
+
+		$user->questions()->create($data);
+
+		return "Data was created";
+    }
 }
